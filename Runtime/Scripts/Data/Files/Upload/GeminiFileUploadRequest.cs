8dbeb3d792734033cb14be25d71cc07f06576eae
Udayshankar Ravikumar <uralstech@gmail.com>
2024-08-26 01:06:33 +0530

    Updated to latest v1Beta features, and restructured project.

---
+++ Runtime/Scripts/Data/Files/Upload/GeminiFileUploadRequest.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.ComponentModel;
+using System.Text;
+
+namespace Uralstech.UGemini.FileAPI
+{
+    /// <summary>
+    /// Uploads a file to the Gemini File API. Response type is <see cref="GeminiFileUploadResponse"/>.
+    /// </summary>
+    /// <remarks>
+    /// This feature is currently being worked on and is unstable.
+    /// </remarks>
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class GeminiFileUploadRequest : IGeminiMultiPartPostRequest
+    {
+        /// <summary>
+        /// Optional metadata for the <see cref="GeminiFile"/> to be uploaded.
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore), DefaultValue(null)]
+        public GeminiFileUploadMetaData File = null;
+
+        /// <summary>
+        /// The IANA standard MIME type of the <see cref="GeminiFileUploadRequest"/>.
+        /// </summary>
+        [JsonIgnore]
+        public string MimeType;
+
+        /// <summary>
+        /// The raw file data to upload.
+        /// </summary>
+        [JsonIgnore]
+        public byte[] RawData;
+
+        /// <summary>
+        /// The API version to use.
+        /// </summary>
+        [JsonIgnore]
+        public string ApiVersion;
+
+        /// <inheritdoc/>
+        [JsonIgnore]
+        public string ContentType { get; }
+
+        /// <inheritdoc/>
+        public string GetEndpointUri(GeminiRequestMetadata metadata)
+        {
+            return $"https://generativelanguage.googleapis.com/upload/{ApiVersion}/files";
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="GeminiFileUploadRequest"/>.
+        /// </summary>
+        /// <param name="contentType">The content type of the data.</param>
+        /// <param name="useBetaApi">Should the request use the Beta API?</param>
+        public GeminiFileUploadRequest(string contentType, bool useBetaApi = true)
+        {
+            ContentType = contentType;
+            ApiVersion = useBetaApi ? "v1beta" : "v1";
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="GeminiFileUploadRequest"/>.
+        /// </summary>
+        /// <param name="contentType">The content type of the data.</param>
+        /// <param name="useBetaApi">Should the request use the Beta API?</param>
+        public GeminiFileUploadRequest(GeminiContentType contentType, bool useBetaApi = true)
+        {
+            ContentType = contentType.MimeType();
+            ApiVersion = useBetaApi ? "v1beta" : "v1";
+        }
+
+        /// <inheritdoc/>
+        public string GetUtf8EncodedData(string dataSeperator)
+        {
+            StringBuilder data = new($"--{dataSeperator}\r\n");
+
+            data.Append("Content-Disposition: form-data; name=\"metadata\"\r\n");
+            data.Append("Content-Type: application/json; charset=UTF-8\r\n\r\n");
+            data.Append($"{JsonConvert.SerializeObject(this)}\r\n");
+
+            data.Append($"--{dataSeperator}\r\n");
+            data.Append("Content-Disposition: form-data; name=\"file\"\r\n");
+            data.Append($"Content-Type: {ContentType}\r\n\r\n");
+            data.Append($"{Encoding.UTF8.GetString(RawData)}\r\n");
+            data.Append($"--{dataSeperator}--\r\n");
+
+            return data.ToString();
+        }
+    }
+}
