2e382e7d8e7c571d71b59a81d5da0239a8db4e05
Udayshankar Ravikumar <uralstech@gmail.com>
2024-09-06 13:53:39 +0530

    Added metadata response type for tuning.

---
+++ Runtime/Scripts/Data/Models/Tuning/Create/GeminiTunedModelCreationOperationMetadata.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Uralstech.UGemini.Models.Tuning
+{
+    /// <summary>
+    /// Metadata about the state and progress of creating a tuned model returned from the long-running operation
+    /// </summary>
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class GeminiTunedModelCreationOperationMetadata
+    {
+        /// <summary>
+        /// Contains a URI identifying the type.
+        /// </summary>
+        [JsonProperty("@type")]
+        public string Type;
+
+        /// <summary>
+        /// The ID of the model being tuned.
+        /// </summary>
+        [JsonConverter(typeof(GeminiModelIdStringConverter))]
+        public GeminiModelId TunedModel;
+
+        /// <summary>
+        /// The total number of tuning steps.
+        /// </summary>
+        public int TotalSteps;
+
+        /// <summary>
+        /// The number of steps completed.
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public int? CompletedSteps = null;
+
+        /// <summary>
+        /// The completed percentage for the tuning operation.
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public float? CompletedPercent = null;
+
+        /// <summary>
+        /// Metrics collected during tuning.
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public GeminiTuningSnapshot[] Snapshots = null;
+    }
+}
